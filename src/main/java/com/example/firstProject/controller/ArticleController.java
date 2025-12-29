eedc97a4159a0d167cd0d34a4e6c09313bbc83b0
jho <zchrx7625@naver.com>
2025-12-29 18:10:59 +0900

    12/29

---
+++ src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -0,0 +1,52 @@
+package com.example.firstProject.controller;
+
+import com.example.firstProject.dto.ArticleForm;
+import com.example.firstProject.entity.Article;
+import com.example.firstProject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Slf4j // 로깅 기능을 위한 어노테이션 추가
+@Controller
+public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+//        System.out.println(form.toString());
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+//        System.out.println(article.toString());
+        log.info(article.toString());
+        // 2. repository 로 entity를 DB에 저장
+        Article saved = articleRepository.save(article);
+//        System.out.println(saved.toString());
+        log.info(saved.toString());
+        return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "";
+    }
+}
